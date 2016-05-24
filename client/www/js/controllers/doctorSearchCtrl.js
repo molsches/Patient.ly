e2559ebd781dec275fdf06ea69d8346b6980ee24
Tegan Duong <teganduong@gmail.com>
2016-05-24 14:53:23 -0700

    Implement rating for each doctor from list of results

--- client/www/js/controllers/doctorSearchCtrl.js
+++ client/www/js/controllers/doctorSearchCtrl.js
@@ -7,17 +7,18 @@ angular.module('app.doctorSearch', [])
 
   $scope.searchDoctors = function() {
     Doctors.searchDoctors($scope.query).then(function(results) {
-      console.log('results.data.data from doc search', results.data.data);
+      console.log('results.data from doc search', results);
       var allResults = results.data.data;
       allResults.forEach(function(doc) {
         var result = {
-          title: doc.profile.first_name + ' ' + doc.profile.last_name + ', MD',
+          title: doc.profile.first_name + ' ' + doc.profile.last_name + ', ' + doc.profile.title,
           bio: doc.profile.bio,
           image: doc.profile.image_url,
           specialty: doc.specialties[0].actor,
           phones: doc.practices[0].phones,
           location: doc.practices[0].visit_address
         };
+        result.rating = doc.ratings.length !== 0 ? doc.ratings[0].rating : 'Not available';
 
         $scope.results.push(result);
       });
