34ef787528371aad823c17c23e9e578cf9110e13
Tegan Duong <teganduong@gmail.com>
2016-05-24 14:21:03 -0700

    Implement searchDoctors and place function on scope to enable search

--- client/www/js/controllers/doctorSearchCtrl.js
+++ client/www/js/controllers/doctorSearchCtrl.js
@@ -1,9 +1,26 @@
 angular.module('app.doctorSearch', [])
 
-.controller('DoctorSearchCtrl', function($scope, Conditions) {
-  // $scope.getAllConditions = function() {
-  //   Conditions.fluTracker().then(function(data) {
-  //     console.log('fluTracker: ', data);
-  //   });
-  // };
+.controller('DoctorSearchCtrl', function($scope, Doctors) {
+
+  $scope.query = {};
+  $scope.results = [];
+
+  $scope.searchDoctors = function() {
+    Doctors.searchDoctors($scope.query).then(function(results) {
+      console.log('results.data.data from doc search', results.data.data);
+      var allResults = results.data.data;
+      allResults.forEach(function(doc) {
+        var result = {
+          title: doc.profile.first_name + ' ' + doc.profile.last_name + ', MD',
+          bio: doc.profile.bio,
+          image: doc.profile.image_url,
+          specialty: doc.specialties[0].actor,
+          phones: doc.practices[0].phones,
+          location: doc.practices[0].visit_address
+        };
+
+        $scope.results.push(result);
+      });
+    });
+  };
 })
