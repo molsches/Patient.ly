c2c555ea83581c804f38ec37ea82c9a77a281798
Tegan Duong <teganduong@gmail.com>
2016-05-23 19:11:54 -0700

    Initialize app and register modules

---
+++ client/www/js/app.js
@@ -0,0 +1,12 @@
+angular.module('app', [
+  'ionic', 
+  'firebase',
+  'app.routes', 
+  'app.services', 
+  'ui.router',
+  'app.medications',
+  'app.home',
+  'app.searchDrugs',
+  'app.conditions'
+])
+
